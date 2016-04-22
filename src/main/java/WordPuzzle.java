0ce80bb538ee7e0490712040c5f417cc1128033e
Luca Quatela <lucaqq@gmail.com>
2016-04-22 11:52:30 -0700

    it will create an array and a string containing the puzzled version of the sentence

--- src/main/java/WordPuzzle.java
+++ src/main/java/WordPuzzle.java
@@ -2,15 +2,49 @@
 // import java.util.HashMap;
 import java.io.*;
 import java.util.ArrayList;
+import java.io.Console;
+
 
 public class WordPuzzle {
+  static Integer counter = 0;
+  static ArrayList<String> puzzledSentence = new ArrayList<String>();
+  // static String testString = "Hello";
+
+  // public static String WorldPuzzler (String userSentence){
+  //   String[] vowels = {"a", "e", "i", "o", "u"};
+  //   for (String vowel: vowels){
+  //     userSentence = userSentence.replace(vowel,"-");
+  //     userSentence = userSentence.replace(vowel.toUpperCase(),"-");
+  //   }
+  //   return userSentence;
+  // }
+
   public static String WorldPuzzler (String userSentence){
-    String[] vowels = {"a", "e", "i", "o", "u"};
-    for (String vowel: vowels){
-      userSentence = userSentence.replace(vowel,"-");
-      userSentence = userSentence.replace(vowel.toUpperCase(),"-");
+    Console myConsole = System.console();
+    String[] vowels = {"a", "e", "i", "o", "u","y"};
+    String[] consonants = {"b", "c", "d", "f", "g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","z"," "};
+    String[] splittedUserSentence = userSentence.split("");
+    String puzzledSentenceString = "";
+
+    for (String letter : splittedUserSentence){
+
+        for (String vowel : vowels){
+          if (letter.contains(vowel) || letter.contains(vowel.toUpperCase())){
+            puzzledSentence.add("-");
+            puzzledSentenceString += "-";
+          }
+        }
+        for (String consonant : consonants){
+          if (letter.contains(consonant) || letter.contains(consonant.toUpperCase())){
+            System.out.println(consonant);
+            System.out.println(letter);
+            System.out.println(puzzledSentenceString);
+            puzzledSentence.add(letter);
+            puzzledSentenceString += letter;
+          }
+        }
     }
-    return userSentence;
+    return puzzledSentenceString;
   }
 
   public static Boolean sentenceChecker (String originalUserSentence ,String userGuess) {
